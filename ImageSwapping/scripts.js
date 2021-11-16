8e9af1e9913bd7285925ae9c886d92bc251a9273
Miriam Vera <vera123@live.missouristate.edu>
2021-11-16 11:05:16 -0600

    Added new images

--- ImageSwapping/scripts.js
+++ ImageSwapping/scripts.js
@@ -2,3 +2,10 @@
 function changeImage(varImage) { 
     document.getElementById('bigImage').src=varImage;
 }
+
+bigImage.addEventListener("dblclick", function() {
+    document.getElementById('img1').src='imagefive.jpg';
+    document.getElementById('img2').src='imagesix.jpeg';
+    document.getElementById('img3').src='imageseven.jpeg';
+    document.getElementById('img4').src='imageeight.jpeg';
+});
